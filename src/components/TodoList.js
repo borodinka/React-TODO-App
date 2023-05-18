b3ac98fd8d458ee97a1df8e434a3852cede3b8fe
borodinka <95708108+borodinka@users.noreply.github.com>
2023-05-18 22:13:35 +0200

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,80 +1,27 @@
-import { useState } from "react";
+import TodoItem from "./TodoItem";
 
 export default function TodoList({ tasks, dispatch }) {
-	const [editingTaskId, setEditingTaskId] = useState(null);
-	const [editedText, setEditedText] = useState("");
-  
-	const handleEdit = (taskId) => {
-	  setEditingTaskId(taskId);
-	  const task = tasks.find((t) => t.id === taskId);
-	  setEditedText(task.text);
-	};
-  
-	const handleSave = () => {
-	  dispatch({
-		type: "UPDATE_TODO",
-		task: { id: editingTaskId, text: editedText },
-	  });
-	  setEditingTaskId(null);
-	};
-
 	return (
-	  <>
-		{tasks.map((task) => (
-		  <li key={task.id}>
-			{editingTaskId === task.id ? (
-			  <>
-				<input
-				  type="text"
-				  value={editedText}
-				  onChange={(e) => setEditedText(e.target.value)}
-				/>
-				<button onClick={handleSave} style={{ margin: 5 }}>Save</button>
-			  </>
-			) : (
-			  <>
-				<input
-				  onChange={() =>
-					dispatch({
-					  type: "UPDATE_TODO",
-					  task: { ...task, done: !task.done },
-					})
-				  }
-				  type="checkbox"
-				  checked={task.done}
-				/>
-				{task.text}
-				<button
-				  onClick={() =>
-					dispatch({
-					  type: "DELETE_TODO",
-					  taskId: task.id,
-					})
-				  }
-				  style={{ margin: 5 }}
-				>
-				  Delete
-				</button>
-				<button onClick={() => handleEdit(task.id)}>
-				  Edit
-				</button>
-			  </>
-			)}
-		  </li>
-		))}
-		<button
-		  onClick={() =>
-			tasks.map((task) =>
-			  dispatch({
-				type: "DELETE_TODO",
-				taskId: task.id,
-			  })
-			)
-		  }
-		  style={{ marginLeft: 15 }}
-		>
-		  Remove all todos
-		</button>
+		<>
+			{tasks.map(task => (
+				<li key={task.id}>
+					<TodoItem 
+						task={task}
+						dispatch={dispatch}
+					/>
+				</li>
+			))}
+			<button onClick={() =>
+				tasks.map((task) =>
+			  		dispatch({
+						type: "DELETE_TODO",
+						taskId: task.id,
+			  		})
+				)
+			}
+		  	style={{ marginLeft: 15 }}>
+		  		Remove all todos
+			</button>
 	  </>
 	);
 }
