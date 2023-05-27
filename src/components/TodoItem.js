1ec9f016eb06706b8ebd50b0fa645ba668a759ea
borodinka <95708108+borodinka@users.noreply.github.com>
2023-05-27 13:09:29 +0200

    Update Delete all & Cleanup

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,63 +1,58 @@
 import { useState } from "react";
 
 export default function TodoItem({ task, dispatch }) {
-	const [isEditing, setIsEditing] = useState(false);
-	let taskContent;
+  const toggleTodoCompleted = () =>
+    dispatch({
+      type: "UPDATE_TODO",
+      task: { ...task, done: !task.done },
+    });
 
-	if (isEditing) {
-		taskContent = (
-			  <>
-				<input
-					value={task.text}
-					onChange={e => {
-						dispatch({
-							type: 'UPDATE_TODO',
-							task: { ...task, text: e.target.value },
-						});
-					}}
-				/>
-				<button onClick={() => 
-					setIsEditing(false)}
-					style={{ margin: 5 }}
-				>
-					Save
-				</button>
-			</>
-		);
-	} else {
-		taskContent = (
-			  <>
-				{task.text}
-				<button onClick={() => 
-					setIsEditing(true)}
-					style={{ margin: 5 }}
-				>
-					Edit
-				</button>
-			</>
-		);
-	}	
+  const handleDeleteTodo = () =>
+    dispatch({
+      type: "DELETE_TODO",
+      taskId: task.id,
+    });
 
-	return (
-		<>
-			<input onChange={() =>
-				dispatch({
-					type: 'UPDATE_TODO',
-					task: { ...task, done: !task.done },
-				})
-			}
-				type="checkbox"
-				checked={task.done}
-			/>
-			{taskContent}
-			<button onClick={() =>
-				dispatch({
-					type: 'DELETE_TODO',
-					taskId: task.id,
-				})
-			}>
-				Delete
-			</button>	
-		</>		
-	);
+  return (
+    <>
+      <input
+        onChange={toggleTodoCompleted}
+        type="checkbox"
+        checked={task.done}
+      />
+      <TaskContent task={task} dispatch={dispatch} />
+      <button onClick={handleDeleteTodo}>Delete</button>
+    </>
+  );
+}
+
+function TaskContent({ task, dispatch }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  if (isEditing) {
+    return (
+      <>
+        <input
+          value={task.text}
+          onChange={(e) => {
+            dispatch({
+              type: "UPDATE_TODO",
+              task: { ...task, text: e.target.value },
+            });
+          }}
+        />
+        <button onClick={() => setIsEditing(false)} style={{ margin: 5 }}>
+          Save
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      {task.text}
+      <button onClick={() => setIsEditing(true)} style={{ margin: 5 }}>
+        Edit
+      </button>
+    </>
+  );
 }
