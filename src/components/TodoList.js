6b698fbc42ae02bdbbce36bb54d6e73f1fac09a2
borodinka <95708108+borodinka@users.noreply.github.com>
2023-05-27 13:10:12 +0200

    Update Delete all

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,23 @@
 import TodoItem from "./TodoItem";
 
 export default function TodoList({ tasks, dispatch }) {
-	return (
-		<>
-			{tasks.map(task => (
-				<li key={task.id}>
-					<TodoItem 
-						task={task}
-						dispatch={dispatch}
-					/>
-				</li>
-			))}
-			<button onClick={() =>
-				tasks.map((task) =>
-			  		dispatch({
-						type: "DELETE_TODO",
-						taskId: task.id,
-			  		})
-				)
-			}
-		  	style={{ marginLeft: 18 }}>
-		  		Remove all todos
-			</button>
-	  </>
-	);
+  return (
+    <>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <TodoItem task={task} dispatch={dispatch} />
+        </li>
+      ))}
+      <button
+        onClick={() =>
+          dispatch({
+            type: "DELETE_ALL",
+          })
+        }
+        style={{ marginLeft: 15 }}
+      >
+        Remove all todos
+      </button>
+    </>
+  );
 }
